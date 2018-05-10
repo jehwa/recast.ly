d0eed16b3217376d7f09cdfcc4f91f2941193c47
jehwa <s.jehwa@gmail.com>
2018-05-09 20:53:53 -0700

    Playing correct video

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,7 +1,7 @@
 var VideoPlayer = (prop) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={prop.video.snippet.thumbnails.default.url} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${prop.video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
       <h3>{prop.video.snippet.title}</h3>
